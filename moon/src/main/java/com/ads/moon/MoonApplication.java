938affd527dc2e3a17065264b0a4d51513ae52e1
p_xiyifeng <xi.yifeng@aliyun.com>
2019-01-08 18:23:49 +0800

    初始化

---
+++ moon/src/main/java/com/ads/moon/MoonApplication.java
@@ -0,0 +1,14 @@
+package com.ads.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonApplication.class, args);
+	}
+
+}
+
